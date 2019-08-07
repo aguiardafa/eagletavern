93ea9b18130ae5de437ece873aa8d3b91b0ff172
aguiardafa <diego.fernandes.aguiar@gmail.com>
2019-08-07 02:01:38 -0300

    Configurando o DispatcherServlet para condificação de caracteres (UTF-8)

--- src/main/java/com/aguiardafa/eagletavern/config/init/AppInitializer.java
+++ src/main/java/com/aguiardafa/eagletavern/config/init/AppInitializer.java
@@ -1,5 +1,8 @@
 package com.aguiardafa.eagletavern.config.init;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import com.aguiardafa.eagletavern.config.WebConfig;
@@ -13,14 +16,27 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 		return null;
 	}
 
+	//informando ao DispatcherServlet a classe do configuração do projeto
+	//onde por exemplo ele vai descobrir quais são os controllers do projeto 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
 		return new Class<?>[] { WebConfig.class };
 	}
 
+	//mapeando para o DispatcherServlet o ponto de partida para captura das requisições
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] { "/" };
+	}
+	
+	//configurando codificação das páginas fornecidas pelo DispatcherServlet do projeto
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		
+		return new Filter[] { characterEncodingFilter };
 	}
 
 }
